f1250dd6b396a5bf438e19ffb27bf033efbbfc05
Hassaan Ali <58141711+hassaanali723@users.noreply.github.com>
2020-11-21 19:26:58 +0500

    added print statement

--- src/ustaad.java
+++ src/ustaad.java
@@ -1,5 +1,7 @@
 public class ustaad {
     public static void main(String[] args) {
         System.out.println("Ustaad is love,will u marry mee");
+        System.out.println("ustaad in love with hassaan");
+
     }
 }
